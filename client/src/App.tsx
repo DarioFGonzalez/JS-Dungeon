83b99015de07bbb25fc8b7d0c811b650ec8272ae
DarioFGonzalez <dario.zerobyte@gmail.com>
2025-04-19 18:02:23 -0300

    Refactoricé la lógica de movimiento del jugador y limpié código legacy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,222 +7,115 @@ const ship_left = '<';
 const ship_right = '>';
 const asteroid = 'e';
 
+type playerData = [ number, number, string ];
+
 const App = () =>
 {
-  const [ mapa, setMapa ] = useState( Array.from( {length: 18}, ()=> Array.from( Array(18), ()=>'' ) ) );
-  const [ dead, setDead ] = useState( false );
-  const [ player, setPlayer ] = useState( [ 0, 0, '^' ] );
+    const [ game, setGame ] = useState<boolean>(false);
+    const [ mapa, setMapa ] = useState( Array.from( {length: 18}, ()=> Array.from( Array(18), ()=> '') ) );
 
-  const handleMovement = (event: React.KeyboardEvent) =>
-  {
-      if(!dead)
-      {
-          let here = [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2) ];
-          let symbol = ship_up;
-          mapa.forEach( (fila, y) => fila.map( (celda, z) =>
-          {
-              if(celda==ship_down || celda==ship_up || celda==ship_left || celda==ship_right ) { here=[ y, z ]; symbol=celda; }
-          } ));
-          setPlayer( [ here[0], here[1], symbol ] );
-          let auxiliar = [ ...mapa ];
-  
-          switch(event.key)
-          {
-              case 'W':
-              case 'w':
-                  if(event.shiftKey)
-                  {
-                      let aux = [ ...mapa ];
-                      aux[here[0]][here[1]]=ship_up;
-                      setPlayer( [ here[0], here[1], ship_up ] );
-                      setMapa(aux);
-                  }
-                  else
-                  {
-                      if(here[0]-1>=0 && mapa[here[0]-1][here[1]]=='')
-                      {
-                          let aux = mapa;
-                          aux[here[0]][here[1]]='';
-                          aux[here[0]-1][here[1]]=ship_up;
-                          setPlayer( [ here[0]-1, here[1], ship_up ] );
-                          setMapa(aux);
-                      }
-                      else
-                      {
-                          if(here[0]-1>=0 && mapa[here[0]-1][here[1]]==asteroid)
-                          {
-                              auxiliar[here[0]][here[1]] = '';
-                              auxiliar[here[0]-1][here[1]] = '💥';
-                              // death();
-                              // setScore( Number(score) - 20 );
-                              auxiliar[ Math.floor(mapa.length/2) ][ Math.floor(mapa[0].length/2) ] = 'v';
-                              setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), 'v' ] );
-                              setMapa(auxiliar);
-                              boomAnimation(here[0]-1, 1, 0, here[1], 1);
-                          }
-                          else
-                          {
-                              let aux = mapa;
-                              aux[here[0]][here[1]]=ship_up;
-                              setPlayer( [ here[0], here[1], ship_up ] );
-                              setMapa(aux);
-                          }
-                      }
-                  }
-                  break;
-              case 'A':
-              case 'a':
-                  if(event.shiftKey)
-                  {
-                      let aux = mapa;
-                      aux[here[0]][here[1]]=ship_left;
-                      setPlayer( [ here[0], here[1], ship_left ] );
-                      setMapa(aux);
-                  }
-                  else
-                  {
-                      if(here[1]-1>=0 && mapa[here[0]][here[1]-1]=='')
-                      {
-                          let aux = mapa;
-                          aux[here[0]][here[1]]='';
-                          aux[here[0]][here[1]-1]=ship_left;
-                          setPlayer( [ here[0], here[1]-1, ship_left ] );
-                          setMapa(aux);
-                      }
-                      else
-                      {
-                          if(here[1]-1>=0 && mapa[here[0]][here[1]-1]==asteroid)
-                          {
-                              auxiliar[here[0]][here[1]] = '';
-                              auxiliar[here[0]][here[1]-1] = '💥';
-                              // death();
-                              // setScore( Number(score) - 20 );
-                              auxiliar[ Math.floor(mapa.length/2) ][ Math.floor(mapa[0].length/2) ] = ship_down;
-                              setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), ship_down ] );
-                              setMapa(auxiliar);
-                              boomAnimation(here[0], 1, 0, here[1]-1, 1);
-                          }
-                          else
-                          {
-                              let aux = mapa;
-                              aux[here[0]][here[1]]=ship_left;
-                              setPlayer( [ here[0], here[1], ship_left ] );
-                              setMapa(aux);
-                          }
-                      }
-                  }
-                  break;
-              case 'S':
-              case 's':
-                  if(event.shiftKey)
-                  {
-                      let aux = mapa;
-                      aux[here[0]][here[1]]=ship_down;
-                      setPlayer( [ here[0], here[1], ship_down ] );
-                      setMapa(aux);
-                  }
-                  else
-                  {
-                      if(here[0]+1<mapa.length && mapa[here[0]+1][here[1]]=='')
-                      {
-                          let aux = mapa;
-                          aux[here[0]][here[1]]='';
-                          aux[here[0]+1][here[1]]=ship_down;
-                          setPlayer( [ here[0]+1, here[1], ship_down ] );
-                          setMapa(aux);
-                      }
-                      else
-                      {
-                          if(here[0]+1<mapa.length && mapa[here[0]+1][here[1]]==asteroid)
-                          {
-                              auxiliar[here[0]][here[1]] = '';
-                              auxiliar[here[0]+1][here[1]] = '💥';
-                              // death();
-                              // setScore( Number(score) - 20 );
-                              auxiliar[ Math.floor(mapa.length/2) ][ Math.floor(mapa[0].length/2) ] = ship_down;
-                              setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), ship_down ] );
-                              setMapa(auxiliar);
-                              boomAnimation(here[0]+1, 1, 0, here[1], 1);
-                          }
-                          else
-                          {
-                              auxiliar[here[0]][here[1]]=ship_down;
-                              setPlayer( [ here[0], here[1], ship_down ] );
-                              setMapa(auxiliar);
-                          }
-                      }
-                  }
-                  break;
-              case 'D':
-              case 'd':
-                  if(event.shiftKey)
-                  {
-                      let aux = mapa;
-                      aux[here[0]][here[1]]=ship_right;
-                      setPlayer( [ here[0], here[1], ship_right ] );
-                      setMapa(aux);
-                  }
-                  else
-                  {
-                      if(here[1]+1<mapa[0].length && mapa[here[0]][here[1]+1]=='')
-                      {
-                          let aux = mapa;
-                          aux[here[0]][here[1]]='';
-                          aux[here[0]][here[1]+1]=ship_right;
-                          setPlayer( [ here[0], here[1]+1, ship_right ] );
-                          setMapa(aux);
-                      }
-                      else
-                      {
-                          if(here[1]+1<mapa[0].length && mapa[here[0]][here[1]+1]==asteroid)
-                          {
-                              auxiliar[here[0]][here[1]] = '';
-                              auxiliar[here[0]][here[1]+1] = '💥';
-                              // death();
-                              // setScore( Number(score) - 20 );
-                              auxiliar[ Math.floor(mapa.length/2) ][ Math.floor(mapa[0].length/2) ] = ship_down;
-                              setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), ship_down ] );
-                              setMapa(auxiliar);
-                              boomAnimation(here[0], 1, 0, here[1]+1, 1);
-                          }
-                          else
-                          {
-                              auxiliar[here[0]][here[1]]=ship_right;
-                              setPlayer( [ here[0], here[1], ship_right ] );
-                              setMapa(auxiliar);
-                          }
-                      }
-                  }
-                  break;
-              default:
-                  break;
-          }
-      }
-  }
+    // const [ dead, setDead ] = useState( false );
+    const [ player, setPlayer ] = useState<playerData>( [ 0, 0, '^' ] );
 
-  const boomAnimation = ( bulletAt0: number, c: number, movimiento: any, bulletAt1: number , interaction: number) =>
+    const findPlayer = () =>
     {
-        let aux = [ ...mapa ];
-        setTimeout( () =>
+        let here = [ 0, 0 ];
+        let symbol = '';
+        mapa.forEach( (fila, y) => fila.map( (celda, z) =>
         {
-            if(interaction == 1)
+            if(celda==ship_down || celda==ship_up || celda==ship_left || celda==ship_right ) { here=[ y, z ]; symbol=celda; }
+        } ));
+        setPlayer( [ here[0], here[1], symbol ] );
+    }
+
+    const movePlayer = ( x: number, y: number, symbol: string ) =>
+    {
+        const newX = player[0]+x;
+        const newY = player[1]+y;
+        if( newX>=0 && newX < mapa.length && newY>=0 && newY < mapa[0].length ) //Dentro del mapa
+        {
+            const auxiliar = mapa.map(fila => [...fila]);
+            const [ pX, pY ] = player;
+            if(mapa[newX][newY] == '')  //Hacia 'VACÍO'.
             {
-                let objective = aux[Number(bulletAt0) + ( c * Number(movimiento) ) ][Number(bulletAt1)];
-                if( !(objective=='<' || objective=='>' || objective=='^' || objective=='v') )
-                {
-                    aux[Number(bulletAt0) + ( c * Number(movimiento) ) ][Number(bulletAt1)] = '';
-                }
+                auxiliar[pX][pY] = '';
+                auxiliar[newX][newY] = symbol;
+                setPlayer( [ newX, newY, symbol ] );
+                setMapa(auxiliar);
             }
-            if(interaction == 2)
+            else    //Ninguna de las anteriores. "No caminable".
             {
-                let objective = aux[Number(bulletAt0)][Number(bulletAt1) + ( c * Number(movimiento) ) ];
-                if( !(objective=='<' || objective=='>' || objective=='^' || objective=='v') )
-                {
-                    aux[Number(bulletAt0)][Number(bulletAt1) + ( c * Number(movimiento) ) ] = '';
-                }
+                auxiliar[pX][pY] = symbol;
+                setPlayer( [ pX, pY, symbol ] );
+                setMapa(auxiliar);
             }
-            setMapa(aux);
-        }, 100);
+        }
+        else
+        {
+            const auxiliar = mapa.map(fila => [...fila]);
+            const [ pX, pY ] = player;
+            auxiliar[pX][pY] = symbol;
+            setPlayer( [ pX, pY, symbol ] );
+            setMapa(auxiliar);
+        }
+    }
+
+    const handleMovement = (event: React.KeyboardEvent) =>
+    {
+        if(game)
+        {
+            findPlayer();
+
+            switch(event.key)
+            {
+                case 'W':
+                case 'w':
+                movePlayer(-1,0,ship_up);
+                break;
+                case 'A':
+                case 'a':
+                movePlayer(0,-1,ship_left);
+                break;
+                case 'S':
+                case 's':
+                movePlayer(+1,0,ship_down);
+                break;
+                case 'D':
+                case 'd':
+                movePlayer(0,+1,ship_right);
+                break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    const loadGame = () =>
+    {
+        const auxiliar = Array.from( {length: 18}, ()=> Array.from( Array(18), ()=> '') );  //Vacía el mapa
+        // for(let i=0; i<18; i++)
+        // {
+        //     auxiliar[i][0]='x';
+        // }
+        auxiliar[Math.floor(mapa.length/2)][Math.floor(mapa[0].length/2)] = ship_up;    //Agrega el jugador al centro
+        setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), ship_up ] );
+        setMapa(auxiliar);
+    }
+
+    const startGame = () =>
+    {
+        loadGame();
+        setGame(true);
+    }
+
+    const stopGame = () =>
+    {
+        findPlayer();
+        const auxiliar = mapa.map(fila => [...fila]);
+        auxiliar[player[0]][player[1]] = '';
+        setPlayer( [ Math.floor(mapa.length/2), Math.floor(mapa[0].length/2), ship_up ] );
+        setMapa(auxiliar);
+        setGame(false);
     }
 
   return(
@@ -232,35 +125,13 @@ const App = () =>
 
                 {mapa.map( ( fila, x ) =>
                 <div key={x} className='fila'>
-                    {fila.map( ( celda, y ) =>
-                    {
-                        let x = false;
-                        let estilo = 'celda';
-                        if(celda=='v' || celda=='>' || celda=='<' || celda=='^' || celda=='e')
-                        {
-                            x = true;
-                        }
-
-                        if(dead && x && celda!='e')
-                        {
-                            estilo = 'titilar';
-                        }
-
-                        return(
-                            <>
-                                { x && <img src={celda} key={y} className={estilo} /> }
-                                { !x && <label key={y} className='celda' >
-                                            {celda}
-                                        </label>}
-                            </>
-                        )
-                    } )}
+                    { fila.map( ( celda, y ) => { return( <label key={y} className='celda'> {celda} </label> ) } ) }
                 </div> )}
 
             </div>
         </div>
-        {/* {startId==0 && <button onClick={startGame}> START </button>}
-        {startId!=0 && <button onClick={stopGame}> STOP </button>} */}
+        {!game && <button onClick={startGame}> START </button>}
+        {game && <button onClick={stopGame}> STOP </button>}
     </div>
   );
 }
