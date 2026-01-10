f00afa377d2c458b0c62fbda8819142c11b75d57
DarioFGonzalez <dario.zerobyte@gmail.com>
2026-01-09 21:46:03 -0300

    fix: re-upload

--- client/src/components/data/entities.ts
+++ client/src/components/data/entities.ts
@@ -88,7 +88,7 @@ export const heavyEnemy: Types.Enemy =
         flags: { Poisoned: false, Bleeding: false, Burning: false },
         instances: { PoisonInstances: [], BleedInstances: [], BurnInstances: [] }
     },
-    attack: { Instant: 3, DoT: 1, Times: 3, Aliment: 'bleed' },
+    attack: { Instant: 3, DoT: 1, Times: 4, Aliment: 'bleed' },
     defense: { Armor: 1, Toughness: 3, Immunity: 'bleed' },
     pattern: 'vertical',
     drops: [ { item: Items.Potion, chance: 99, quantity: 2 }, { item: Gear.Dagger1, chance: 99, quantity: 1 }, { item: Items.Bandages, chance: 50, quantity: 2 } ]
